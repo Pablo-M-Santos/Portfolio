ab85d883d7953ce1eba64d765be29310578f0b45
Pablo Santos <pablomoreirasantos.hp@gmail.com>
2023-12-18 23:33:56 -0300

    Portfólio - Projetos

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ let navLinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
+        let offset = sec.offsetTop - 120;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
