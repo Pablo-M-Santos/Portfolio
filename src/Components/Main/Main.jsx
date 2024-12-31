b6c3b761927e66d87b366ca5582618a7ba308891
Pablo <pablomoreirasantos.hp@gmail.com>
2024-12-30 23:49:23 -0300

    Home finalizada e responsiva

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,4 +1,7 @@
 import './Main.css';
+import fotoPerfil from '../../assets/foto-perfil-nova.png';
+import { FaWhatsapp, FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { MdOutlineMail } from "react-icons/md";
 import { useEffect } from 'react';
 
 const Main = () => {
@@ -30,9 +33,31 @@ const Main = () => {
 
   return (
     <div className="main-container">
-      <section className="section" id="home">
-        Home
+      <section className="home section" id="home">
+        <div className="home-container">
+          <img src={fotoPerfil} alt="Pablo Santos" className="home-img" />
+          <div className="home-texto">
+            <h1>Olá, eu sou<br/><strong id="home-nome">Pablo Santos,</strong></h1>
+            <h2>sou <span> desenvolvedor Full-Stack.</span></h2>
+
+            <div className="social-media">
+              <a href="https://wa.me/558592822527?text=Olá!,%20eu%20vim%20do%20seu%20Portfólio"><FaWhatsapp /></a>
+              <a href="https://github.com/Pablo-M-Santos"><FaGithub /></a>
+              <a href="https://www.linkedin.com/in/pablo-m-santos/"><FaLinkedinIn /></a>
+              <a href="mailto:pablomoreirasantos.hp@gmail.com"><MdOutlineMail /></a>
+            </div>
+            <a className="btn" href="https://drive.google.com/file/d/1qj3IlZ9TbRHWs4KhQoSqtOfE2VHC3zUj/view?usp=sharing" target="_blank">Currículo</a>
+          </div>
+        </div>
       </section>
+
+
+
+
+
+
+
+      
       <section className="section" id="quem-sou">
         Quem Sou
       </section>
