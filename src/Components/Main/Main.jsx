2b8015720603abd465d7fe64ed374d71ac363188
Pablo <pablomoreirasantos.hp@gmail.com>
2025-01-04 15:58:10 -0300

    feat: Experiência Responsiva

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -35,25 +35,25 @@ const Main = () => {
   const [sliderPosition, setSliderPosition] = useState(0);
   const experiences = {
     wda: {
-      title: "Desenvolvedor Full-Stack",
-      date: "Jul 2024 - Dez 2024 (6 meses)",
-      description:
-        "Estagiei como desenvolvedor full-stack na WDA Tecnologia & Inovação, onde criei uma aplicação web voltada para a gestão de uma locadora de livros.",
-      tools: ["HTML", "CSS", "JavaScript"],
+      titulo: "Desenvolvedor Full-Stack",
+      data: "Jul 2024 - ATUALMENTE",
+      descricao:
+        "Atualmente, estou estagiando como desenvolvedor full-stack na WDA Tecnologia & Inovação. Participei no desenvolvimento da aplicação de gerenciamento de livros, com consumo de API. Minha atuação incluiu o desenvolvimento de interfaces responsivas e a implementação de funcionalidades no back-end, focando na otimização de processos e na integração eficiente com a API para uma melhor experiência do usuário.",
+      ferramentas: ["Notion", "GitLab", "Figma", "Vue", "Spring", "Java", "Postman", "Docker"],
     },
     freelancer: {
-      title: "Desenvolvedor Frontend",
-      date: "Ago 2020 - Ago 2021 (1 ano)",
-      description:
-        "Atuei como freelancer desenvolvendo projetos de sites responsivos e interfaces dinâmicas para pequenas empresas.",
-      tools: ["React", "Tailwind", "Next.js"],
+      titulo: "Desenvolvedor Freelancer",
+      data: "JAN. 2023 - NOV. 2024 ( 1 ano )",
+      descricao:
+        "Atuei como desenvolvedor freelancer, sendo responsável pela criação de pequenos sites e projetos, incluindo e-commerces e lojas online. Durante esse período, trabalhei no planejamento, desenvolvimento e lançamento de sites, garantindo que fossem funcionais e alinhados às necessidades dos clientes. Além disso, sempre busquei proporcionar uma experiência do usuário intuitiva e eficiente.",
+      ferramentas: ["HTML", "CSS", "JavaScript", "Figma", "Gitlab", "Wordpress"],
     },
-    estagio: {
-      title: "Estagiário Full-Stack",
-      date: "Jan 2019 - Dez 2019 (1 ano)",
-      description:
-        "Experiência inicial em desenvolvimento de software, trabalhando em APIs REST e integração de sistemas com tecnologias modernas.",
-      tools: ["Node.js", "Express", "MongoDB"],
+    eeep: {
+      titulo: "Desenvolvedor de Software",
+      data: "Jan 2022 - Dez 2024 ( 3 ano )",
+      descricao:
+        "Durante minha trajetória na Escola Estadual de Ensino Profissionalizante Luiza de Teodoro Vieira, tive a oportunidade de aprender novas ferramentas e foi o meu ponto de partida na área de programação. Desenvolvi pequenos projetos pessoais tanto no front-end quanto no back-end, adquirindo conhecimentos em ambas as áreas e aplicando-os em soluções práticas para aprimorar minhas habilidades técnicas.",
+      ferramentas: ["HTML", "CSS", "JavaScript", "Figma", "Vue", "Bootstrap", "Python", "PHP", "Java"],
     },
   };
 
@@ -100,74 +100,51 @@ const Main = () => {
 
         {/* Experiencia */}
         <section className="experiencia">
-        <h2 className="titulo-experiencia">Experiências</h2>
-        <div className="container-experiencia">
-          <ul className="experiencia-opcoes">
-          <div className="barra-deslizante" style={{ top: `${sliderPosition * 72}px` }}  ></div>
-            {Object.keys(experiences).map((key, index) => (
-              <li
-                key={key}
-                className={activeExperience === key ? "active-experiencia" : ""}
-                onClick={() => handleExperienceChange(key, index)}
-              >
-                {key.toUpperCase()}
-              </li>
-            ))}
-          </ul>
-          <div className="detalhes">
-            <div className="title-row">
-              <h2 className="titulo-card">{experiences[activeExperience].title}</h2>
-              <span className="data">{experiences[activeExperience].date}</span>
-            </div>
-            <p>{experiences[activeExperience].description}</p>
-            <div className="ferramentas-experiencia">
-              <h3 className="titulo-ferramenta">Ferramentas Utilizadas:</h3>
-              <div className="icons">
-                {experiences[activeExperience].tools.map((tool, index) => (
-                  <div className="icon" key={index} title={tool}>
-                    <img
-                      align="center"
-                      title={tool}
-                      alt={tool}
-                      height="40"
-                      width="50"
-                      src={`https://skillicons.dev/icons?i=${tool.toLowerCase()}`}
-                    />
-                  </div>
-                ))}
+          <h2 className="titulo-experiencia">Experiências</h2>
+          <div className="container-experiencia">
+            <ul className="experiencia-opcoes">
+            <div className="barra-deslizante" style={{ top: `${sliderPosition * 72}px` }}  ></div>
+              {Object.keys(experiences).map((key, index) => (
+                <li
+                  key={key}
+                  className={activeExperience === key ? "active-experiencia" : ""}
+                  onClick={() => handleExperienceChange(key, index)}
+                >
+                  {key.toUpperCase()}
+                </li>
+              ))}
+            </ul>
+            <div className="detalhes">
+              <div className="title-row">
+                <h2 className="titulo-card">{experiences[activeExperience].titulo}</h2>
+                <span className="data">{experiences[activeExperience].data}</span>
+              </div>
+              <p>{experiences[activeExperience].descricao}</p>
+              <div className="ferramentas-experiencia">
+                <h3 className="titulo-ferramenta">Ferramentas Utilizadas:</h3>
+                <div className="icons">
+                  {experiences[activeExperience].ferramentas.map((tool, index) => (
+                    <div className="icon" key={index} title={tool}>
+                      <img
+                        align="center"
+                        title={tool}
+                        alt={tool}
+                        height="40"
+                        width="50"
+                        src={`https://skillicons.dev/icons?i=${tool.toLowerCase()}`}
+                      />
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </section>
       </section>
+      <section className="formacao section" id="formacao">
+        <div className="waveBottom"></div>
       </section>
 
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      <section className="section" id="formacao">
-        Formação
-      </section>
       <section className="section" id="projetos">
         Projetos
       </section>
